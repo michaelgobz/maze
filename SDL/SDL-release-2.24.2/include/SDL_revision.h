c9c869a6922bd56233afa1d00d2ef86650cf786f
michaelgobz <raniahgobz@outlook.com>
2022-11-06 08:09:33 -0800

    SDL2

--- SDL/SDL-release-2.24.2/include/SDL_revision.h
+++ SDL/SDL-release-2.24.2/include/SDL_revision.h
@@ -1,2 +1,2 @@
-#define SDL_REVISION ""
+#define SDL_REVISION "https://github.com/michaelgobz/maze.git@3d2332ea9023e0386b2e68fb22acdff2166880b6 b0e101ee62825d12e5f858655e729b693ea19472"
 #define SDL_REVISION_NUMBER 0
